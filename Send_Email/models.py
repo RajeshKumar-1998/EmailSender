67a95db4b81d093035ffde9412c266947f260bfe
Rajesh kumar A <57509232+Rajeshkumar-dev-cloud@users.noreply.github.com>
2020-05-31 19:25:37 +0530

    Five Commmit

--- Send_Email/models.py
+++ Send_Email/models.py
@@ -4,8 +4,8 @@ from django.db import models
 # Create your models here.
 
 class Email_Model(models.Model):
-    sub = models.CharField(max_length=10000,blank=False)
-    mes = models.CharField(max_length=100000,blank=False)
+    sub = models.TextField(max_length=10000,blank=False)
+    mes = models.TextField(max_length=100000,blank=False)
 
     def __str__(self):
         return self.sub + self.mes
